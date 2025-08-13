a2082b1dc675624671e06b76bc1c9e2c864b033c
Asilbek <asilbekegamnazarov999@gmail.com>
2025-08-14 01:39:47 +0500

    add

--- data/categories.js
+++ data/categories.js
@@ -45,99 +45,141 @@ module.exports = [
         ]
 
     },
+
     {
         id: 10004,
         title: "Maishiy texnika",
+        img: `${BASE_URL}/maishiyTexnika.svg`,
+        children: []
 
     },
+
     {
         id: 10014,
         title: "Kiyim",
+        img: `${BASE_URL}/kiyim.svg`,
+        children: []
 
     },
+
     {
         id: 10013,
         title: "Poyabzallar",
+        img: `${BASE_URL}/poyabzal.svg`,
+        children: []
 
     },
+
     {
         id: 10003,
         title: "Aksessuarlar",
+        img: `${BASE_URL}/aksessuarlar.svg`,
+        children: []
 
     },
     {
         id: 10012,
         title: "Go'zallik va parvarish",
+        img: `${BASE_URL}/guzallikVaParvarish.svg`,
+        children: []
 
     },
     {
         id: 10009,
         title: "Salomatlik",
+        img: `${BASE_URL}/salomatlik.svg`,
+        children: []
 
     },
     {
         id: 10018,
         title: "Uy-ro'zg'or buyumlari",
+        img: `${BASE_URL}/uyRozgor.svg`,
+        children: []
 
     },
     {
         id: 10016,
         title: "Qurilish va ta'mirlash",
+        img: `${BASE_URL}/qurilish.svg`,
+        children: []
 
     },
     {
         id: 10002,
         title: "Avtotovarlar",
+        img: `${BASE_URL}/autoTovarlar.svg`,
+        children: []
 
     },
     {
         id: 10007,
         title: "Bolalar tovarlari",
+        img: `${BASE_URL}/bolalarTovarlari.svg`,
+        children: []
 
     },
     {
         id: 10008,
         title: "Xobbi va ijod",
+        img: `${BASE_URL}/xobbiVaIjod.svg`,
+        children: []
 
     },
     {
         id: 10015,
         title: "Sport va hordiq",
+        img: `${BASE_URL}/sportvaHordiq.svg`,
+        children: []
 
     },
     {
         id: 1821,
         title: "Oziq-ovqat mahsulotlari",
+        img: `${BASE_URL}/oziqOvqat.svg`,
+        children: []
 
     },
     {
         id: 10005,
         title: "Maishiy kimyoviy moddalar",
+        img: `${BASE_URL}/maishiyKimyoviyModdalar.svg`,
+        children: []
 
     },
     {
         id: 10010,
         title: "Kanselyariya tovarlari",
+        img: `${BASE_URL}/kanselyariyaTovarlari.svg`,
+        children: []
 
     },
     {
         id: 10019,
         title: "Hayvonlar uchun tovarlar",
+        img: `${BASE_URL}/hayvonlarTovarlari.svg`,
+        children: []
 
     },
     {
         id: 10011,
         title: "Kitoblar",
+        img: `${BASE_URL}/kitoblar.svg`,
+        children: []
 
     },
     {
         id: 10006,
         title: "Dacha, bog' va tomorqa",
+        img: `${BASE_URL}/dacha.svg`,
+        children: []
 
     },
     {
         id: 12087,
         title: "Realibitatsiya uchun subsidiyalangan mahsulotlar",
+        img: `${BASE_URL}/salomatlik.svg`,
+        children: []
 
     },
 ]
