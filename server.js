186916a37d22b684e7eebee11626d01c0a0b506c
Asilbek <asilbekegamnazarov999@gmail.com>
2025-08-13 10:50:51 +0500

    add

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.use("/api/products", productsRoute)
 app.use("/api/categories", categoriesRoute)
