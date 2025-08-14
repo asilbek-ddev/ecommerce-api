41ce138f3da625169a565efc0d7615830de3a3cb
Asilbek <asilbekegamnazarov999@gmail.com>
2025-08-14 18:11:57 +0500

    add

--- data/slider.js
+++ data/slider.js
@@ -4,11 +4,171 @@ module.exports = [
     {
         id: 15152,
         link: "https://uzum.uz/uz/promo/freetime",
-        img: `${BASE_URL}/banners/main_page_banner.png`
+        img: `${BASE_URL}/banners/main_page_banner1.png`
     },
+
     {
         id: 15154,
-        link: "",
+        link: "/category/garantiya-nizkikh-cen",
         img: `${BASE_URL}/banners/main_page_banner_2.png`
-    }
+    },
+
+    {
+        id: 15134,
+        link: "https://b.2u.uz/marketbanner",
+        img: `${BASE_URL}/banners/main_page_banner_3.png`
+    },
+
+    {
+        id: 52656,
+        link: "/roduct/napitok-gazirovanny-sting-3000-ml",
+        img: `${BASE_URL}/banners/main_page_banner_4.png`
+    },
+
+    {
+        id: 97452,
+        link: "/shop/oilatanlovikiyim",
+        img: `${BASE_URL}/banners/main_page_banner_5.png`
+    },
+
+    {
+        id: 23656,
+        link: "https://promo.maped.uz/",
+        img: `${BASE_URL}/banners/main_page_banner_6.png`
+    },
+
+    {
+        id: 98532,
+        link: "/category/neoline-darit-podarki",
+        img: `${BASE_URL}/banners/main_page_banner_7.png`
+    },
+
+    {
+        id: 15454,
+        link: "/category/tureckaya-posuda",
+        img: `${BASE_URL}/banners/main_page_banner_8.png`
+    },
+
+    {
+        id: 21540,
+        link: "/category/skidki-na-crocs",
+        img: `${BASE_URL}/banners/main_page_banner_9.png`
+    },
+
+    {
+        id: 69852,
+        link: "/category/mark-ryden",
+        img: `${BASE_URL}/banners/main_page_banner_10.png`
+    },
+
+    {
+        id: 31024,
+        link: "/category/oila-tanlovi",
+        img: `${BASE_URL}/banners/main_page_banner_11.png`
+    },
+    {
+        id: 76052,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_12.png`
+    },
+    {
+        id: 41035,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_13.png`
+    },
+    {
+        id: 97036,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_14.png`
+    },
+    {
+        id: 10354,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_15.png`
+    },
+    {
+        id: 84035,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_16.png`
+    },
+    {
+        id: 75843,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_17.png`
+    },
+    {
+        id: 29343,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_18.png`
+    },
+    {
+        id: 29487,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_19.png`
+    },
+    {
+        id: 48382,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_20.png`
+    },
+    {
+        id: 49382,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_21.png`
+    },
+    {
+        id: 29480,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_22.png`
+    },
+    {
+        id: 93847,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_23.png`
+    },
+    {
+        id: 20839,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_24.png`
+    },
+    {
+        id: 10838,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_25.png`
+    },
+    {
+        id: 39483,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_26.png`
+    },
+    {
+        id: 53415,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_27.png`
+    },
+    {
+        id: 75522,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_28.png`
+    },
+    {
+        id: 60345,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_29.png`
+    },
+    {
+        id: 32354,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_30.png`
+    },
+    {
+        id: 62341,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_31.png`
+    },
+    {
+        id: 26215,
+        link: "",
+        img: `${BASE_URL}/banners/main_page_banner_32.png`
+    },
 ]
