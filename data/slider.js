b5e4bfa96daf774e15db4c0eb08525de416de381
Asilbek <asilbekegamnazarov999@gmail.com>
2025-08-14 16:04:04 +0500

    add

--- data/slider.js
+++ data/slider.js
@@ -4,7 +4,7 @@ module.exports = [
     {
         id: 15152,
         link: "https://uzum.uz/uz/promo/freetime",
-        img: `${BASE_URL}/heroBanners/main_page_banners.png`
+        img: `${BASE_URL}/main_page_banner.png`
     },
     {
         id: 15154,
