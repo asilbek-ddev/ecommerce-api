f1eee0be48bd14157282422cf33c13dc0c92ee28
Asilbek <asilbekegamnazarov999@gmail.com>
2025-08-14 16:01:23 +0500

    add

--- data/slider.js
+++ data/slider.js
@@ -1,3 +1,14 @@
+const BASE_URL = "https://uzum-face-api.vercel.app"
+
 module.exports = [
-    
+    {
+        id: 15152,
+        link: "https://uzum.uz/uz/promo/freetime",
+        img: `${BASE_URL}/heroBanners/main_page_banners.png`
+    },
+    {
+        id: 15154,
+        link: "",
+        img: ""
+    }
 ]
